0285396e0b53faae9d90e02d0867ca397a4ca829
adamfisk <a@littleshoot.org>
2008-05-12 23:43:51 +0000

    cleaned up formatting
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/download@3052 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/download/DownloaderListener.java
+++ src/main/java/org/lastbamboo/common/download/DownloaderListener.java
@@ -3,17 +3,14 @@ package org.lastbamboo.common.download;
 /**
  * A listener for downloaders.
  * 
- * @param <StateT>
- *      The downloader state type.
+ * @param <StateT> The downloader state type.
  */
 public interface DownloaderListener<StateT>
     {
     /**
      * Notification that the downloader state has changed.
      * 
-     * @param state
-     *      The new downloader state.
+     * @param state The new downloader state.
      */
-    void stateChanged
-            (StateT state);
+    void stateChanged (StateT state);
     }
