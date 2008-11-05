c1d44df5a3b4d0d2d9b49124d2d05c59a02a224f
adamfisk <a@littleshoot.org>
2008-11-05 18:17:07 +0000

    fixed imports
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/download@4117 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/download/MultiSourceDownloader.java
+++ src/main/java/org/lastbamboo/common/download/MultiSourceDownloader.java
@@ -14,7 +14,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
 
 import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
